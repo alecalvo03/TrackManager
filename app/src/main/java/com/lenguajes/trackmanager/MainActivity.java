f52b7cc529ec51e1b375ae34d7c5772a4895a1a7
alecalvo03 <ale_calvo03@outlook.com>
2018-06-15 17:25:24 -0600

    Connect and Track Edit
    The connect and track edit screens are separated. Edit screen can set starting point and clear the whole matrix

--- app/src/main/java/com/lenguajes/trackmanager/MainActivity.java
+++ app/src/main/java/com/lenguajes/trackmanager/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 }*/
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,50 +23,38 @@ import java.io.IOException;
 
 public class MainActivity extends Activity {
 
-    TextView response;
-    EditText editTextAddress, editTextPort;
-    Button buttonConnect, buttonClear;
-    GridLayout gridLayout;
-    ToggleButton btnGrid[][];
-    Client myClient;
+    private TextView response;
+    private EditText editTextAddress, editTextPort;
+    private Button buttonConnect, buttonClear, clrButton, startButton;
+    private GridLayout gridLayout;
+    private ToggleButton test;
+    private ToggleButton btnGrid[][];
+    private Client myClient;
+    private View connectForm;
+    private View trackForm;
+    private int startcoord[] = {-1,-1};
+
+    private boolean toStart = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextAddress = (EditText) findViewById(R.id.addressEditText);
-        editTextPort = (EditText) findViewById(R.id.portEditText);
-        buttonConnect = (Button) findViewById(R.id.connectButton);
-        buttonClear = (Button) findViewById(R.id.clearButton);
-        response = (TextView) findViewById(R.id.responseTextView);
+        editTextAddress = findViewById(R.id.addressEditText);
+        editTextPort = findViewById(R.id.portEditText);
+        buttonConnect = findViewById(R.id.connectButton);
+        buttonClear = findViewById(R.id.clearButton);
+        response = findViewById(R.id.responseTextView);
         gridLayout = findViewById(R.id.gridlayout);
-        btnGrid = new ToggleButton[4][4];
-        editTextAddress.setText("192.168.100.2");
+        editTextAddress.setText("192.168.100.3");
         editTextPort.setText("9090");
 
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++){
-                final int finalJ = j;
-                final int finalI = i;
-                btnGrid[i][j] = new ToggleButton(this);
-                GridLayout.LayoutParams params = new GridLayout.LayoutParams(
-                        GridLayout.spec(j,1),GridLayout.spec(i));
-                gridLayout.addView(btnGrid[i][j], i);
-                btnGrid[i][j].setLayoutParams(params);
-                btnGrid[i][j].setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        try {
-                            myClient.sendToServer(finalI + "-" + finalJ);
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
+        connectForm = findViewById(R.id.connect_form);
+        trackForm = findViewById(R.id.track_form);
 
-                    }
-                });
-            }
-        }
+        final int col = gridLayout.getColumnCount();
+        final int row = gridLayout.getRowCount();
 
         buttonConnect.setOnClickListener(new OnClickListener() {
 
@@ -75,6 +64,9 @@ public class MainActivity extends Activity {
                     myClient = new Client(editTextAddress.getText().toString(),
                             Integer.parseInt(editTextPort.getText().toString()),
                             response);
+                    connectForm.setVisibility(View.GONE);
+                    trackForm.setVisibility(View.VISIBLE);
+                    initGrid(col,row);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -87,6 +79,80 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 response.setText("");
+            }
+        });
+    }
+
+    private void setStart(int i, int j) {
+        if (btnGrid[i][j].isChecked()) {
+            btnGrid[i][j].setChecked(false);
+        } else {
+            if (startcoord[0] != -1) {
+                btnGrid[startcoord[0]][startcoord[1]].setBackground(btnGrid[i][j].getBackground());
+            }
+            btnGrid[i][j].setChecked(true);
+            btnGrid[i][j].setBackgroundColor(Color.RED);
+            startcoord[0] = i;
+            startcoord[1] = j;
+        }
+    }
+
+    private void clearMatrix(){
+        for (ToggleButton btn[] : btnGrid){
+            for (ToggleButton tbtn : btn){
+                tbtn.setChecked(false);
+            }
+        }
+        btnGrid[startcoord[0]][startcoord[1]].setBackground(btnGrid[startcoord[0]+1][0].getBackground());
+    }
+
+    private void initGrid(int col, int row){
+        btnGrid = new ToggleButton[col][row];
+        for (int i = 0; i < col; i++){
+            for (int j = 0; j < row; j++){
+                final int finalJ = j;
+                final int finalI = i;
+                btnGrid[i][j] = new ToggleButton(this);
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams(
+                        GridLayout.spec(i),GridLayout.spec(j));
+                params.width = 100;
+                params.height = 100;
+                gridLayout.addView(btnGrid[i][j], params);
+                btnGrid[i][j].setText("");
+                btnGrid[i][j].setTextOff("");
+                btnGrid[i][j].setTextOn("");
+                btnGrid[i][j].setBackgroundResource(R.drawable.select_button);
+                //btnGrid[i][j].setLayoutParams(params);
+                btnGrid[i][j].setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            myClient.sendToServer(finalI + "-" + finalJ);
+                            if (toStart){
+                                setStart(finalI,finalJ);
+                                toStart = false;
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+            }
+        }
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toStart = true;
+            }
+        });
+
+        clrButton = findViewById(R.id.clrButton);
+        clrButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearMatrix();
             }
         });
     }
